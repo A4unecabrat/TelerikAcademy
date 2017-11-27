d988d90bc158aa5b83d2b1eea3d1071e47f5902d
Angel Novoselski <angelkun@abv.bg>
2017-11-27 13:43:25 +0200

    Added solution for Parse Tags

--- Strings Training/Parse tags/Parse tags/Program.cs
+++ Strings Training/Parse tags/Parse tags/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Parse_tags
@@ -11,7 +12,13 @@ namespace Parse_tags
         static void Main(string[] args)
         {
             string input = Console.ReadLine();
-
+            string pattern = @"<upcase>(.*?)<\/upcase>";
+            MatchCollection result = Regex.Matches(input, pattern);
+            foreach (Match item in result)
+            {
+                input = input.Replace(item.Value, item.Groups[1].ToString().ToUpper());
+            }
+            Console.WriteLine(input);
         }
     }
 }
